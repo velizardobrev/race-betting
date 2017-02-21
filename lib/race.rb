5e944b53fb78e7896e95126f309c260888bce199
Velizar Dobrev <velizardobrev@gmail.com>
2017-02-21 12:43:08 +0200

    version 5.0 complete

--- lib/race.rb
+++ lib/race.rb
@@ -2,24 +2,11 @@ module RaceBet
   class Race
 
     class << self
-      #guesses = [:joe, :mary, :bob, :sheldon, :howard, :frank]
-      #winners = [:mary, :bob, :sheldon, :howard, :frank]
       
       def score(guesses, winners)
         ##################################################################################
-        #first 5 elements of both arrays
-        winnerstemp = winners[0..4]
-        guessestemp = guesses[0..4]
-
-        #logical flags for result
-        keyFirst = false
-        keySecond = false
-        keyThird = false
-        keyForth = false
-        keyFifth = false
-        keyScore = false
-        #keyZero = false
-        #helping variables
+        #variables       
+        keys= Array.new(6,false) #logical flags for result
         score = 1
         total = 0
         counter = 0
@@ -27,82 +14,71 @@ module RaceBet
         j=1
         ##################################################################################
         # Main logic
-      
-               
+                    
         #correct first place
         if guesses[0]==winners[0] and guesses[0].nil? == false and winners[0].nil? == false       
-         keyFirst = true 
+         keys[1]=true 
         end
         #correct 2nd place
         if guesses[1]==winners[1] and guesses[1].nil? == false and winners[1].nil? == false
-          keySecond = true
+          keys[2]=true 
         end
         #correct 3rd place
         if guesses[2]==winners[2] and guesses[2].nil? == false and winners[2].nil? == false
-          keyThird = true
+          keys[3]=true
         end
         #correct 4th place
         if guesses[3]==winners[3] and guesses[3].nil? == false and winners[3].nil? == false
-          keyForth = true
+          keys[4]=true
         end
         #correct 5th place      
         if guesses[4]==winners[4] and guesses[4].nil? == false and winners[4].nil? == false
-          keyFifth = true
+          keys[5]=true
         end
         
            #gives one point for a correct guess in the wrong place
           
-
-           guesses.length.times do |i|
+           winners.length.times do |i|
             winners.length.times do |j|
-              if guessestemp[i-1]==winnerstemp[j-1] and i!=j and guessestemp[i-1].nil? == false and winnerstemp[i-1].nil? == false    
-                #Below is output for console testing
-                #puts"I GIVE ONE to the item below"
-                keyScore = true
-                counter = counter + 1                
-
-               
-              end
-                #Below is output for console testing
-                #puts"#{i} #{j}"
-                #puts"#{guessestemp[i-1]} == #{winnerstemp[j-1]}"
- 
+              if guesses[i-1]==winners[j-1] and i!=j and 
+                guesses[i-1].nil? == false and winners[j-1].nil? == false and 
+                guesses[i-1] != "loser" and winners[j-1] != "loser" 
+                #end of logical condition  
+                keys[0]=true
+                counter = counter + 1
+                                             
+              end 
             end
           end       
 
-          
- 
         ##################################################################################
         #Return value logic
-        if  keyFirst == true
+        if  keys[1] == true
           total = total + 15
         end
-        if keySecond == true
+        if keys[2] == true
           total = total + 10
         end
-        if keyThird == true
+        if keys[3] == true
           total = total + 5
         end
-        if keyForth == true
+        if keys[4] == true
           total = total + 3
         end                  
-        if keyFifth == true
+        if keys[5] == true
           total = total + 1
         end
-        if keyScore == true
+        if keys[0] == true
           total = total + counter*score
         end
         #Returning value
-        total          
+        total
 
-        end#score         
+        end#score
 
-
-      
       end #class
-        
+
   end #class
-  
 
 end#module
 
