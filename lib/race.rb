9e7cfd3451245cc79d9a21643a908e925c752af5
Velizar Dobrev <velizardobrev@gmail.com>
2017-02-23 19:31:49 +0200

    version 8 --method

--- lib/race.rb
+++ lib/race.rb
@@ -10,23 +10,29 @@ module RaceBet
         n=0
         ##################################################################################
         # Main logic
+        #subscore method
+        def subScore(n)
+          subTotal=0
+          case 
+           when n==0 #first place
+            subTotal = subTotal + 15
+           when n==1 #second place 
+            subTotal = subTotal + 10
+           when n==2 #3rd place  
+            subTotal = subTotal + 5 
+           when n==3 #4th place
+            subTotal = subTotal + 3
+           when n==4 #5th place
+            subTotal = subTotal + 1 
+           else    
+          end #case
+        end #subscore
+
         guesses.each_with_index do |guessValue, guessIndex|
           winners.each_with_index do |winValue, winIndex|
             5.times do |n|
               if guessIndex == n and winIndex == n and guessValue == winValue
-                case 
-                  when n==0 #first place
-                    total = total + 15
-                  when n==1 #second place 
-                    total = total + 10
-                  when n==2 #3rd place  
-                    total = total + 5 
-                  when n==3 #4th place
-                    total = total + 3
-                  when n==4 #5th place
-                    total = total + 1 
-                  else    
-                end #case
+                total = total + subScore(n)
               end #if
             end #do
             #missplaced elements
